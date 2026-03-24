07cead973a5e33e42a0df6190219641c6f604df1
Pratham <jadhavprathmesh284@gmail.com>
2026-03-24 10:31:01 +0530

    .env

--- src/main/java/com/example/LoanManagementApp/config/AwsConfig.java
+++ src/main/java/com/example/LoanManagementApp/config/AwsConfig.java
@@ -2,23 +2,21 @@ package com.example.LoanManagementApp.config;
 
 import com.amazonaws.auth.*;
 import com.amazonaws.services.s3.*;
-import org.springframework.beans.factory.annotation.Value;
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.context.annotation.*;
 
 @Configuration
 public class AwsConfig {
 
-    @Value("${aws.accessKey}")
-    private String accessKey;
-
-    @Value("${aws.secretKey}")
-    private String secretKey;
-
-    @Value("${aws.region}")
-    private String region;
+    private final Dotenv dotenv = Dotenv.load();
 
     @Bean
     public AmazonS3 amazonS3() {
+
+        String accessKey = dotenv.get("AWS_ACCESS_KEY");
+        String secretKey = dotenv.get("AWS_SECRET_KEY");
+        String region = dotenv.get("AWS_REGION");
+
         BasicAWSCredentials creds = new BasicAWSCredentials(accessKey, secretKey);
 
         return AmazonS3ClientBuilder.standard()
