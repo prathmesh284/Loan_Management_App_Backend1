7dd11582284a32f68b156ac426c6a84ac8be1ada
Pratham <jadhavprathmesh284@gmail.com>
2026-04-19 01:14:09 +0530

    loan management app v2

--- src/main/java/com/example/LoanManagementApp/controller/BranchController.java
+++ src/main/java/com/example/LoanManagementApp/controller/BranchController.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -44,8 +44,8 @@ public class BranchController {
      * @param branchId Branch ID
      * @return Branch details
      */
-    @GetMapping("/{branchId}")
-    public ResponseEntity<?> getBranchById(@PathVariable Long branchId) {
+    @GetMapping("/details")
+    public ResponseEntity<?> getBranchById(@RequestParam Long branchId) {
         Optional<Branch> branch = branchRepo.findById(branchId);
         if (branch.isEmpty()) {
             return ResponseEntity.notFound().build();
