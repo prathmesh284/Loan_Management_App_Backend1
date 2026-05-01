510c1aa060c7775fb81362914ba62de03fe49d14
Pratham <jadhavprathmesh284@gmail.com>
2026-05-01 20:29:10 +0530

    loan-management-app-v3

--- src/main/java/com/example/LoanManagementApp/service/CustomerService.java
+++ src/main/java/com/example/LoanManagementApp/service/CustomerService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.server.ResponseStatusException;
 import com.example.LoanManagementApp.model.Customer;
 import com.example.LoanManagementApp.repo.CustomerRepo;
@@ -66,6 +67,7 @@ public class CustomerService {
         return repo.findByBranchId(branchId);
     }
 	
+	@Transactional
 	public void deleteByCustomerId(String customerId) {
         if (!repo.existsByCustomerId(customerId)) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Customer not found");
