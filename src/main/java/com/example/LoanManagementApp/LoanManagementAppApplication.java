d89327d3cc06ace2fc8f41d2b45681b72ade8913
Pratham <jadhavprathmesh284@gmail.com>
2026-06-02 13:00:28 +0530

    Loan Management App v3

--- src/main/java/com/example/LoanManagementApp/LoanManagementAppApplication.java
+++ src/main/java/com/example/LoanManagementApp/LoanManagementAppApplication.java
@@ -7,7 +7,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableScheduling
 public class LoanManagementAppApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(LoanManagementAppApplication.class, args);
 	}
